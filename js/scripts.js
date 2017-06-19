58608894cbf0c7ec5d0b8fbbefc1d7376eebf39e
mhismail3 <mhismail3@gmail.com>
2017-06-18 22:59:31 -0500

    Base commit

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+jQuery(document).ready(function ($) {
+
+
+    $(window).stellar();
+    
+    $(window).resize(function() {
+        $.stellar('refresh');
+    });
+    
+//    $.backstretch([
+//        "images/bg-1.jpg",
+//        "images/bg-2.jpg",
+//        "images/bg-3.jpeg"
+//    ], {duration: 10000, fade: 1500});
+
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+    $('img.fade-in-on-load').animate({
+        opacity: 1
+    }, 700);
+    window.setTimeout(function() {
+        $('ul.fade-in-on-load').animate({
+            opacity: 1
+        }, 700);
+        $('h1.fade-in-on-load').animate({
+            opacity: 1
+        }, 700);
+    }, 800);
+    
+    
+    
+    
+    slide.waypoint(function (event, direction) {
+
+        dataslide = $(this).attr('data-slide');
+
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+
+    });
+ 
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+
+
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });
+
+
+});
